d2d4e071ab1219f99591348a386f05c37f7d8ab5
Your Name <you@example.com>
2026-04-16 20:45:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,12 +68,12 @@ void insertMap(HashMap * map, char * key, void * value) {
     }
     if(map->buckets[pos] == NULL ){
         map->buckets[pos] = (Pair*)malloc(sizeof(Pair));
-        strcpy(map->buckets[pos]->key,key);
+        map->buckets[pos]->key = key;
         map->buckets[pos]->value = value;
         map->size += 1;
     }
     else if(map->buckets[pos]->key == NULL ){
-        strcpy(map->buckets[pos]->key,key);
+        map->buckets[pos]->key = key;
         map->buckets[pos]->value = value;
     }
     else if(is_equal(key, map->buckets[pos]->key) == 1){
